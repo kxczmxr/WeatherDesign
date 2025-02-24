49682c6b66d9b7e22a337e48d7a255a65113663f
kxczmxr <jakubkaczmarski4@gmail.com>
2025-02-24 17:03:52 +0100

    Changed visual part of app

--- src/assets/ListIcon.jsx
+++ src/assets/ListIcon.jsx
@@ -2,9 +2,9 @@ const ListIcon = () => {
     return (
         <svg height={'11px'} width={'46px'} id="eNlGI7CrcAE1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
              viewBox="0 0 46 13" shapeRendering="geometricPrecision" textRendering="geometricPrecision">
-            <ellipse rx="6.45" ry="6.45" transform="translate(6.45 6.5)" fill="#fff"/>
-            <ellipse rx="6.45" ry="6.45" transform="translate(23 6.5)" fill="#fff"/>
-            <ellipse rx="6.45" ry="6.45" transform="translate(39.55 6.45)" fill="#fff"/>
+            <ellipse rx="6.25" ry="6.25" transform="translate(6.45 6.5)" fill="#d9d9d9"/>
+            <ellipse rx="6.25" ry="6.25" transform="translate(23 6.5)" fill="#d9d9d9"/>
+            <ellipse rx="6.25" ry="6.25" transform="translate(39.55 6.45)" fill="#d9d9d9"/>
         </svg>
     )
 }
